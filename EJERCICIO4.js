4e30ba3b473b50eb96f2673cac3a266bf03b3744
JavierGonzalez <23308051220738@cbtis122.edu.mx>
2025-09-08 21:35:57 -0600

    EJERCICIO4.js

---
+++ EJERCICIO4.js
@@ -0,0 +1,8 @@
+// Ejercicio 4: Encontrando el Primer Escudo con Poder Suficiente// Instrucciones: Encuentra el primer escudo con un nivel de resistencia mayor a 75.const escudos = [20, 60, 45, 80, 90, 30];// Escribe tu solución aquí usando find():const asdg = escudos.find(num => num > 75);console.log(escudos)console.log(asdg)// Ejercicio 4: Encontrando el Primer Escudo con Poder Suficiente
+// Instrucciones: Encuentra el primer escudo con un nivel de resistencia mayor a 75.
+const escudos = [20, 60, 45, 80, 90, 30];
+// Escribe tu solución aquí usando find():
+const mayor75 = escudos.find(num => num > 75);
+
+console.log(escudos)
+console.log(mayor75)
